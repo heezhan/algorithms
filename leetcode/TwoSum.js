147763ffcbbee66b4719b579f21425b02b64dc88
heezhan <hhan1194@gmail.com>
2020-01-20 19:59:53 -0500

    twosum solution2

--- leetcode/TwoSum.js
+++ leetcode/TwoSum.js
@@ -9,5 +9,23 @@ var twoSum = function(nums, target) {
     }
 };
 
-twoSum([2,7,11,15], 9)
+var twoSum = function(nums, target) {
 
+    obj = {}
+
+    for ( i = 0; i < nums.length; i++ ) {
+        obj[ nums[i] ] = i
+    } //create an object with keys representing the numbers in the array and the corresponding values representing the indices of those numbers 
+
+    for ( i = 0; i < nums.length; i++ ) {
+        let complement = target - nums[i]
+        if ( obj[complement] ) {
+        return [ obj[nums[i]], obj[complement] ]
+        } else {
+        return "n/a"
+        }
+    } //reference the object instead of the array to find 1. the existence of a complementary value, and if true, 2. the indices of the twoSum pair
+
+}
+
+twoSum([2,3,11,15], 9)
