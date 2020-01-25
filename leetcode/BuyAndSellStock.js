a3303881bacfcbc56c4a307078c31bc32fc9d559
heezhan <hhan1194@gmail.com>
2020-01-25 18:32:38 -0500

    buy and sell stock

--- leetcode/BuyAndSellStock.js
+++ leetcode/BuyAndSellStock.js
@@ -1,20 +1,13 @@
-// solution 1: brute force
-
 var maxProfit = function(prices) {
-  
-  let profitsArr = []
-
-  for (i = 0; i < prices.length; i++) {
-    for (j = i+1 ; j < prices.length; j++)
-    profitsArr.push(prices[i] - prices[j])
-  }
-
-  let profit = Math.min(...profitsArr)
-  
-  if (profit > 0) {
-    return 0
-  } else {
-    return Math.abs(profit)
-  }
-
+    let min = prices[0]
+    let maxProf = 0
+    
+    for (i = 1; i < prices.length; i++) {
+        min = Math.min(min, prices[i])
+    
+        maxProf = Math.max(maxProf, prices[i] - min)
+    }
+    
+    return maxProf
 };
+
--- leetcode/TwoSum.js
+++ leetcode/TwoSum.js
@@ -13,5 +13,3 @@ var twoSum = function(nums, target) {
         }
     }
 }
-
-twoSum([2,7,11,15], 9)
