e67fef3899957491d08c47651dc83f9f8877a1ce
heezhan <hhan1194@gmail.com>
2020-01-25 21:04:55 -0500

    product of array except self

--- LeetCode/Array/ContainsDuplicate.js
+++ LeetCode/Array/ContainsDuplicate.js
@@ -2,7 +2,7 @@
 
 // Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 
-//solution 1
+// solution 1
 var containsDuplicate = function(nums) {
     let obj = {}
 
