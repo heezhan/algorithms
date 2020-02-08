e807c456625124ee22183a003530b05b977ea55f
heezhan <hhan1194@gmail.com>
2020-02-07 20:26:34 -0500

    supermarket done

--- Skilled/Supermarket.js
+++ Skilled/Supermarket.js
@@ -1,6 +1,26 @@
 myList = ["flour", "egg", "egg", "vanilla"]
-inventory = ["flour", "egg", "vanilla"]
+inventory = ["flour", "egg", "egg", "vanilla"]
 
 var supermarket = function(myList, inventory) {
+    let object = {}
+
+    for ( let i = 0; i < inventory.length; i++ ) {
+      if ( object.hasOwnProperty(inventory[i]) ) {
+        object[inventory[i]] += 1
+      } else {
+        object[inventory[i]] = 1
+      }
+    }
+
+    for ( let i = 0; i < myList.length; i++ ) {
+      if ( object.hasOwnProperty([myList[i]]) && object[[myList[i]]] > 0 ) {
+        object[myList[i]] -= 1
+      } else {
+        return false
+      }
+    }
     
+    return true 
 };  
+
+supermarket(myList, inventory)
