c4e586eca9c0748cac24d6a8918be2b9ae2d4a6a
heezhan <hhan1194@gmail.com>
2020-03-19 20:19:34 -0400

    done

---
+++ LeetCode/DynamicProgramming/ClimbingStairs.js
@@ -0,0 +1,19 @@
+//You are climbing a stair case. It takes n steps to reach to the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+var climbStairs = function(n) {      
+    let arr = [1, 1]
+    
+    if (n > 1) {
+      for (let i = 0; i < n-1; i++) {
+        let nextNum = arr[i] + arr[i+1]
+        arr.push(nextNum)
+      } 
+    return arr[n]
+    } else if (n === 1) {
+        return arr[n]
+    } else {
+        return "n/a"
+    }
+}
