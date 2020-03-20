ea74ba01af97b4af2b65b5af493410825508d2c2
heezhan <hhan1194@gmail.com>
2020-03-19 20:33:51 -0400

    saved memory by .1

--- LeetCode/DynamicProgramming/ClimbingStairs.js
+++ LeetCode/DynamicProgramming/ClimbingStairs.js
@@ -6,11 +6,42 @@ var climbStairs = function(n) {
     let arr = [1, 1]
     
     if (n > 1) {
-      for (let i = 0; i < n-1; i++) {
-        let nextNum = arr[i] + arr[i+1]
-        arr.push(nextNum)
-      } 
-    return arr[n]
+        for (let i = 0; i < n-1; i++) {
+            let nextNum = arr[i] + arr[i+1]
+            arr.push(nextNum)
+        } 
+        return arr[n]
+    } else if (n === 1) {
+        return arr[n]
+    } else {
+        return "n/a"
+    }
+}
+
+var climbStairs = function(n) {      
+    let arr = [1, 1]
+    
+    if (n > 1) {
+        for (let i = 0; i < n-1; i++) {
+            let nextNum = arr[i] + arr[i+1]
+            arr.push(nextNum)
+        } 
+        return arr[n]
+    } else if (n === 1) {
+        return arr[n]
+    } else {
+        return "n/a"
+    }
+}
+
+var climbStairs = function(n) {      
+    let arr = [1, 1]
+    
+    if (n > 1) {
+        for (let i = 2; i <= n; i++) {
+            arr[i] = arr[i-1] + arr[i-2]
+        } 
+        return arr[n]
     } else if (n === 1) {
         return arr[n]
     } else {
