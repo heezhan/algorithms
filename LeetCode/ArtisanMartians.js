eb40daed8d4376aecfc60970bff0278854ed7258
heezhan <hhan1194@gmail.com>
2020-03-25 23:38:55 -0400

    cleaner code

--- LeetCode/ArtisanMartians.js
+++ LeetCode/ArtisanMartians.js
@@ -1,16 +1,44 @@
 var artisanMartians = function(target) {
     let insta = {i: 1, n: 1, s: 1, t: 1, a: 2, g: 1, r: 1, m: 1}
     let hash = {}
-  
+
     let arr = target.split(" ")
-  
+
     for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr[i].length; j++) {
+        for (let j = 0; j < arr[i].length; j++) {
         if (hash[arr[i][j]]) {
-          hash[arr[i][j]] += 1
+            hash[arr[i][j]] += 1
         } else {
-          hash[arr[i][j]] = 1
+            hash[arr[i][j]] = 1
         }
+        }
+    }
+
+    let hashKeysArr = Object.keys(hash)
+
+    let count = []
+
+    for (let k = 0; k < hashKeysArr.length; k++) {
+        count.push(Math.ceil(hash[hashKeysArr[k]] / insta[hashKeysArr[k]]))
+    }
+
+    return Math.max(...count) 
+};
+  
+artisanMartians("tiara")
+
+//cleaner code 
+var artisanMartians = function(target) {
+    let insta = {i: 1, n: 1, s: 1, t: 1, a: 2, g: 1, r: 1, m: 1}
+    let hash = {}
+  
+    let str = target.split(" ").join("")
+  
+    for (let i = 0; i < str.length; i++) {
+      if (hash[str[i]]) {
+        hash[str[i]] += 1
+      } else {
+        hash[str[i]] = 1
       }
     }
   
@@ -18,11 +46,11 @@ var artisanMartians = function(target) {
   
     let count = []
   
-    for (let k = 0; k < hashKeysArr.length; k++) {
-      count.push(Math.ceil(hash[hashKeysArr[k]] / insta[hashKeysArr[k]]))
+    for (let j = 0; j < hashKeysArr.length; j++) {
+      count.push(Math.ceil(hash[hashKeysArr[j]] / insta[hashKeysArr[j]]))
     }
   
     return Math.max(...count) 
 };
   
-artisanMartians("tiara")
+artisanMartians("artisan martians")
